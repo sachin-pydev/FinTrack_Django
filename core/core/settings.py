860e633d1babaa970db027ba406a399625fc7610
unknown <sachinprajapati77249@gmail.com>
2025-11-14 05:03:59 +0530

    Fix setting

--- core/core/settings.py
+++ core/core/settings.py
@@ -30,9 +30,11 @@ env = environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Load secret key and debug from environment
+
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
